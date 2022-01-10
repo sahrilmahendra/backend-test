d48cf587c7da697335b43e4edfe156d9d1b68ebf
Sahril Mahendra <sahrilmahendra@gmail.com>
2022-01-10 13:10:05 +0700

    add log middleware

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"erajaya/config"
+	"erajaya/middlewares"
+	"erajaya/routes"
+)
+
+// function main
+func main() {
+	config.InitDB()
+	e := routes.New()
+	// take notes http method activity
+	middlewares.LogMiddlewares(e)
+	// start on port 8080
+	e.Logger.Fatal(e.Start(":8080"))
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,24 @@
 package routes
+
+import (
+	"erajaya/controllers"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// function routes
+func New() *echo.Echo {
+
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+	}))
+
+	// route products without auth
+	e.POST("/products", controllers.AddProductController)
+
+	return e
+}
